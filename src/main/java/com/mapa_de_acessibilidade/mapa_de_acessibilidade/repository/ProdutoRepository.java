7232f5e021095694d259c091df291418038cd476
Hevellyn Monteiro Medeiros <158319704+HevellynMont@users.noreply.github.com>
2025-10-30 10:10:36 -0300

    Add comments to ProdutoRepository for clarity
    Added comments explaining the purpose of the ProdutoRepository.

--- src/main/java/com/mapa_de_acessibilidade/mapa_de_acessibilidade/repository/ProdutoRepository.java
+++ src/main/java/com/mapa_de_acessibilidade/mapa_de_acessibilidade/repository/ProdutoRepository.java
@@ -6,3 +6,7 @@ import com.mapa_de_acessibilidade.mapa_de_acessibilidade.model.Produto;
 
 public interface ProdutoRepository extends JpaRepository<Produto, Long> {
 }
+
+// repositório Produto que estende JpaRepository para fornecer operações CRUD
+// para a entidade Produto, esse arquivo e responsavel por indiretamente se
+// comunicar com o banco de dados, ele que cria as queries a partir dos seus métodos.
